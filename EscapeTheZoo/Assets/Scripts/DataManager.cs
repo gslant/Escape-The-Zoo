848c1762abbc151a5066d115d40febbb56a969e0
gslant <77141272+gslant@users.noreply.github.com>
2022-09-05 13:02:39 +1200

    added DataManager
    player class added, with simple getters and setters for fields

---
+++ EscapeTheZoo/Assets/Scripts/DataManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class DataManager : MonoBehaviour
+{
+    public void SaveData()
+    {
+
+    }
+}
+
+[Serializable]
+
+public class Player
+{
+    public string name;
+    public int balance;
+    public List<string> ownedCosmetics;
+
+    public void setName(string name)
+    {
+        this.name = name;
+    }
+
+    public string getName()
+    {
+        return this.name;
+    }
+
+    public void deductFromBalance(int amount)
+    {
+        this.balance -= amount;
+    }
+
+    public void addToBalance(int amount)
+    {
+        this.balance += amount;
+    }
+
+    public List<string> getOwnedCosmetics()
+    {
+        return this.ownedCosmetics;
+    }
+
+
+    public void giveCosmetic(string cosmeticName)
+    {
+        this.ownedCosmetics.Add(cosmeticName);
+    }
+
+}
+
