15b203f7d3c1d9a3fff10a0519992cb1b069d3c4
Coen <71311597+IceMilkTee@users.noreply.github.com>
2022-09-18 09:14:49 +1200

    Dice and movement working
    Got it working on a basic level just need to implement different tiles ect

---
+++ EscapeTheZoo/Assets/Scripts/GameControl.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+    private static GameObject player1, player2;
+
+    public static int diceSideThrown = 0;
+    public static int player1StartWaypoint = 0;
+    public static int player2StartWaypoint = 0;
+
+    public static bool gameOver = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        player1 = GameObject.Find("Player1");
+        player2 = GameObject.Find("Player2");
+
+        player1.GetComponent<PlayerMovement>().myTurn = false;
+        player2.GetComponent<PlayerMovement>().myTurn = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player1.GetComponent<PlayerMovement>().waypointIndex >
+            player1StartWaypoint + diceSideThrown)
+        {
+            player1.GetComponent<PlayerMovement>().myTurn = false;
+            player1StartWaypoint = player1.GetComponent<PlayerMovement>().waypointIndex - 1;
+        }
+
+        if (player2.GetComponent<PlayerMovement>().waypointIndex >
+            player2StartWaypoint + diceSideThrown)
+        {
+            player2.GetComponent<PlayerMovement>().myTurn = false;
+            player2StartWaypoint = player2.GetComponent<PlayerMovement>().waypointIndex - 1;
+        }
+
+        if (player1.GetComponent<PlayerMovement>().waypointIndex ==
+            player1.GetComponent<PlayerMovement>().waypoints.Length)
+        {
+            gameOver = true;
+        }
+
+        if (player2.GetComponent<PlayerMovement>().waypointIndex ==
+            player2.GetComponent<PlayerMovement>().waypoints.Length)
+        {
+            gameOver = true;
+        }
+    }
+
+    public static void MovePlayer(int playerToMove)
+    {
+        switch (playerToMove)
+        {
+            case 1:
+                player1.GetComponent<PlayerMovement>().myTurn = true;
+                break;
+
+            case 2:
+                player2.GetComponent<PlayerMovement>().myTurn = true;
+                break;
+        }
+    }
+}
--- EscapeTheZoo/Assets/Scripts/PlayerMovement.cs
+++ EscapeTheZoo/Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -23,17 +23,28 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        Debug.Log(myTurn);
         if (myTurn)
             Move();
     }
 
     private void Move()
     {
-        if(waypointIndex <= waypoints.Length - 1)
+        Debug.Log(waypointIndex);
+        Debug.Log(waypoints.Length - 1);
+        Debug.Log(waypoints[waypointIndex]);
+        if (waypointIndex <= waypoints.Length - 1)
         {
             transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed*Time.deltaTime);
-            
-            if(transform.position == waypoints[waypointIndex].transform.position)
+            Debug.Log(transform.position.x);
+            Debug.Log(transform.position.y);
+            Debug.Log(transform.position.z);
+            Debug.Log(waypoints[waypointIndex].transform.position.x);
+            Debug.Log(waypoints[waypointIndex].transform.position.y);
+            Debug.Log(waypoints[waypointIndex].transform.position.z);
+            Debug.Log(waypoints[waypointIndex].transform.position.Equals(transform.position));
+
+            if (transform.position.x == waypoints[waypointIndex].transform.position.x && transform.position.y == waypoints[waypointIndex].transform.position.y)
             {
                 waypointIndex += 1;
             }
