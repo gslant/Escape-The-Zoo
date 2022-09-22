52be62ecc4a042350488b5ceef2c0dc664e49188
dtm3080 <dtm3080@autuni.ac.nz>
2022-09-22 21:42:43 +1200

    minigame and powerups now work just need to implement it into the game

--- EscapeTheZoo/Assets/Scripts/PlayerMovement.cs
+++ EscapeTheZoo/Assets/Scripts/PlayerMovement.cs
@@ -23,26 +23,26 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        Debug.Log(myTurn);
+ //       Debug.Log(myTurn);
         if (myTurn)
             Move();
     }
 
     private void Move()
     {
-        Debug.Log(waypointIndex);
+ /*       Debug.Log(waypointIndex);
         Debug.Log(waypoints.Length - 1);
-        Debug.Log(waypoints[waypointIndex]);
+        Debug.Log(waypoints[waypointIndex]);*/
         if (waypointIndex <= waypoints.Length - 1)
         {
             transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed*Time.deltaTime);
-            Debug.Log(transform.position.x);
-            Debug.Log(transform.position.y);
-            Debug.Log(transform.position.z);
-            Debug.Log(waypoints[waypointIndex].transform.position.x);
-            Debug.Log(waypoints[waypointIndex].transform.position.y);
-            Debug.Log(waypoints[waypointIndex].transform.position.z);
-            Debug.Log(waypoints[waypointIndex].transform.position.Equals(transform.position));
+            //Debug.Log(transform.position.x);
+            //Debug.Log(transform.position.y);
+            //Debug.Log(transform.position.z);
+            //Debug.Log(waypoints[waypointIndex].transform.position.x);
+            //Debug.Log(waypoints[waypointIndex].transform.position.y);
+            //Debug.Log(waypoints[waypointIndex].transform.position.z);
+            //Debug.Log(waypoints[waypointIndex].transform.position.Equals(transform.position));
 
             if (transform.position.x == waypoints[waypointIndex].transform.position.x && transform.position.y == waypoints[waypointIndex].transform.position.y)
             {
