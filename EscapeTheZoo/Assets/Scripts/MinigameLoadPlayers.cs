6cc3ea5b106f623158af2bea6ae1c778a8fe5a71
alessandra-h <84106206+alessandra-h@users.noreply.github.com>
2022-09-23 15:18:41 +1200

    Squashed commit of the following:
    commit 35ba7fa05109341265dc8df06ff3742481fbf872
    Author: alessandra-h <84106206+alessandra-h@users.noreply.github.com>
    Date:   Fri Sep 23 13:42:46 2022 +1200
    
        Change minigame end text to the winning player and losing player
    
    commit 1f3aedcfccd6016fa3b51916ab476c3fe6ef451b
    Author: alessandra-h <84106206+alessandra-h@users.noreply.github.com>
    Date:   Fri Sep 23 13:04:34 2022 +1200
    
        Add text to specify who's player 1 or 2 for clarity
    
    commit aa8fdc5dc0460ac28b3a07a68fe41a4a2b0aaa0c
    Author: alessandra-h <84106206+alessandra-h@users.noreply.github.com>
    Date:   Fri Sep 23 12:35:05 2022 +1200
    
        Resolve merge conflicts with EscapeTheLions.unity file
    
    commit 304870722335d9da7ca868c2d9d33717d7516633
    Merge: d3be116 a0e198a
    Author: alessandra-h <84106206+alessandra-h@users.noreply.github.com>
    Date:   Fri Sep 23 12:21:08 2022 +1200
    
        Merge branch 'main' into nametags
    
    commit d3be116eaa71bfa90a7bbc22497554b95ceaf54d
    Author: alessandra-h <84106206+alessandra-h@users.noreply.github.com>
    Date:   Fri Sep 23 11:07:09 2022 +1200
    
        Change scale of nametags and position in Escape The Lions
    
    commit 78b0667adbc010cfdec08a14351798f3e6bc1b0a
    Author: alessandra-h <84106206+alessandra-h@users.noreply.github.com>
    Date:   Fri Sep 23 10:56:57 2022 +1200
    
        Add loading player profiles to the Escape The Lions minigame
    
        Also added the nametags to the player objects to this scene.
    
    commit e88832a972b555fc4129b41ed1e6a27037dfbf87
    Author: alessandra-h <84106206+alessandra-h@users.noreply.github.com>
    Date:   Fri Sep 23 10:27:22 2022 +1200
    
        Change nametag position and add main camera to nametag canvases in gameboard
    
    commit 4e7120c2cddac86cd2825393296a67f0039588cd
    Merge: 5e32bec 3614638
    Author: alessandra-h <84106206+alessandra-h@users.noreply.github.com>
    Date:   Fri Sep 23 10:19:15 2022 +1200
    
        Merge branch 'main' into nametags
    
    commit 5e32bec103090185c87298eecb14e0fd69e748e5
    Author: alessandra-h <84106206+alessandra-h@users.noreply.github.com>
    Date:   Thu Sep 22 20:22:48 2022 +1200
    
        Add dynamic nametag to players in the game board
    
        Also changed the layer order of the WinCanvas and NameTagCanvas.

---
+++ EscapeTheZoo/Assets/Scripts/MinigameLoadPlayers.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MinigameLoadPlayers : MonoBehaviour
+{
+    // This script can be attached to a game object to load players in from the lobby
+
+    [SerializeField] private GameObject player1, player2;
+    [SerializeField] private GameObject player1Accessory, player2Accessory;
+
+    // Load player variables
+    private static List<Player> listOfPlayersPlaying; // This will load in the selected players from the lobby
+    private List<GameObject> playerObjects;
+    private List<GameObject> playerAccessoryObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetPlayerObjectLists();
+        SetSelectedPlayers();
+        LoadPlayerDataToPlayerObjects();
+    }
+
+    void SetPlayerObjectLists()
+    {
+        playerObjects = new List<GameObject>() { player1, player2 };
+        playerAccessoryObjects = new List<GameObject>() { player1Accessory, player2Accessory };
+    }
+
+    void SetSelectedPlayers()
+    {
+        listOfPlayersPlaying = LobbyManager.getListOfPlayersPlaying();
+    }
+
+    void LoadPlayerDataToPlayerObjects()
+    {
+        for (int i = 0; i < listOfPlayersPlaying.Count; i++)
+        {
+            playerObjects[i].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Character Face Sprites/" + listOfPlayersPlaying[i].getAnimal());
+            playerObjects[i].GetComponentInChildren<TextMeshProUGUI>().text = "<size=60%>(Player " + (i + 1) + ")</size>\n" + listOfPlayersPlaying[i].getName();
+            playerAccessoryObjects[i].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Cosmetic Sprites/" + listOfPlayersPlaying[i].getAccessory());
+        }
+    }
+
+    public static List<Player> GetListOfPlayersPlaying()
+    {
+        return listOfPlayersPlaying;
+    }
+}
