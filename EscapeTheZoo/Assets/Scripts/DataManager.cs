0d9337962f52aee887b72c3de19ebd8a110345ff
gslant <77141272+gslant@users.noreply.github.com>
2022-10-08 10:30:32 +1300

    Update DataManager.cs
    using PlayerPrefs instead of BinaryFormatter

--- EscapeTheZoo/Assets/Scripts/DataManager.cs
+++ EscapeTheZoo/Assets/Scripts/DataManager.cs
@@ -42,6 +42,81 @@ public class DataManager : MonoBehaviour
         file.Close();
     }
 
+    public void saveDataPrefs(Player p1, Player p2)
+    {
+        string play1 = "player1";
+        string play2 = "player2";
+        PlayerPrefs.SetString("name" + play1, p1.getName());
+        PlayerPrefs.SetString("name" + play2, p2.getName());
+
+        PlayerPrefs.SetInt("totalBalance" + play1, p1.getTotalBalance());
+        PlayerPrefs.SetInt("totalBalance" + play2, p2.getTotalBalance());
+
+        PlayerPrefs.SetInt("gameBalance" + play1, p1.getGameBalance());
+        PlayerPrefs.SetInt("gameBalance" + play2, p2.getGameBalance());
+
+        PlayerPrefs.SetString("animal" + play1, p1.getAnimal());
+        PlayerPrefs.SetString("animal" + play2, p2.getAnimal());
+
+        PlayerPrefs.SetString("accessory" + play1, p1.getAccessory());
+        PlayerPrefs.SetString("accessory" + play2, p2.getAccessory());
+
+        PlayerPrefs.SetInt("ownedCosmeticsCount" + play1, p1.getOwnedCosmetics().Count);
+        PlayerPrefs.SetInt("ownedCosmeticsCount" + play2, p2.getOwnedCosmetics().Count);
+
+        for(int i = 0; i < p1.getOwnedCosmetics().Count; i++)
+        {
+            PlayerPrefs.SetString("ownedCosmetics" + play1 + i, p1.getOwnedCosmetics()[i]);
+        }
+
+        for (int i = 0; i < p2.getOwnedCosmetics().Count; i++)
+        {
+            PlayerPrefs.SetString("ownedCosmetics" + play2 + i, p2.getOwnedCosmetics()[i]);
+        }
+    }
+
+    public List<Player> LoadDataPrefs()
+    {
+        List<Player> playerList = new List<Player>();
+        string play1 = "player1";
+        string play2 = "player2";
+
+        string pName = PlayerPrefs.GetString("name" + play1);
+        int totalBalance = PlayerPrefs.GetInt("totalBalance" + play1);
+        int gameBalance = PlayerPrefs.GetInt("gameBalance" + play1);
+        string animal = PlayerPrefs.GetString("animal" + play1);
+        string accessory = PlayerPrefs.GetString("accessory" + play1);
+        int cosmeticsCount = PlayerPrefs.GetInt("ownedCosmeticsCount" + play1);
+
+        List<string> ownedCosmetics = new List<string>();
+        for (int i = 0; i < cosmeticsCount; i++)
+        {
+            ownedCosmetics.Add(PlayerPrefs.GetString("ownedCosmetics" + play1 + i));
+        }
+
+        Player player1 = new Player(pName, totalBalance, animal, accessory, ownedCosmetics);
+
+        pName = PlayerPrefs.GetString("name" + play2);
+        totalBalance = PlayerPrefs.GetInt("totalBalance" + play2);
+        gameBalance = PlayerPrefs.GetInt("gameBalance" + play2);
+        animal = PlayerPrefs.GetString("animal" + play2);
+        accessory = PlayerPrefs.GetString("accessory" + play2);
+        cosmeticsCount = PlayerPrefs.GetInt("ownedCosmeticsCount" + play2);
+
+        ownedCosmetics = new List<string>();
+        for (int i = 0; i < cosmeticsCount; i++)
+        {
+            ownedCosmetics.Add(PlayerPrefs.GetString("ownedCosmetics" + play2 + i));
+        }
+
+        Player player2 = new Player(pName, totalBalance, animal, accessory, ownedCosmetics);
+
+        playerList.Add(player1);
+        playerList.Add(player2);
+
+        return playerList;
+    }
+
     //Load list of players from file
     public List<Player> LoadData(string fileName)
     {
