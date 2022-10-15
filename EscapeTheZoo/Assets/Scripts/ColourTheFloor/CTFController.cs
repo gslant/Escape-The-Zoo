df42bf29fd3515fd962b695f57b48e0edc696882
gslant <77141272+gslant@users.noreply.github.com>
2022-10-15 16:29:39 +1300

    floor coloring working

--- EscapeTheZoo/Assets/Scripts/ColourTheFloor/CTFController.cs
+++ EscapeTheZoo/Assets/Scripts/ColourTheFloor/CTFController.cs
@@ -12,10 +12,20 @@ public class CTFController : MonoBehaviour
 
     public CTFPlayerController player1;
     public CTFPlayerController player2;
+
+    public GameObject grid;
+    public GameObject squarePrefab;
     // Start is called before the first frame update
     void Start()
     {
         GameOverCanvas.SetActive(false);
+        for (int j = -7; j < 8; j += 2)
+        {
+            for (int i = -3; i <= 4; i += 2)
+            {
+                Instantiate(squarePrefab, new Vector3(j, i, 0), Quaternion.identity);
+            }
+        }
     }
 
     // Update is called once per frame
--- EscapeTheZoo/Assets/Scripts/ColourTheFloor/FloorSquare.cs
+++ EscapeTheZoo/Assets/Scripts/ColourTheFloor/FloorSquare.cs
@@ -22,7 +22,13 @@ public class FloorSquare : MonoBehaviour
     {
         if(collision.CompareTag("Player 1"))
         {
+            Debug.Log("Hit Player 1");
             sprite.material.color = Color.red;
+        }
+        if (collision.CompareTag("Player 2"))
+        {
+            Debug.Log("Hit Player 2");
+            sprite.material.color = Color.green;
         }
     }
 }
