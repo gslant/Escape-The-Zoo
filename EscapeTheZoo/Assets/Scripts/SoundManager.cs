7ef99a99f74ee333408d566f2fdf5cb1f5140c62
cns3382 <cns3382@autuni.ac.nz>
2022-10-19 01:42:59 +1300

    Finished the volume settings, and added most of the credits

--- EscapeTheZoo/Assets/Scripts/SoundManager.cs
+++ EscapeTheZoo/Assets/Scripts/SoundManager.cs
@@ -1,30 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class SettingsManager : MonoBehaviour
+public class SoundManager : MonoBehaviour
 {
     // Objects
     public Transform soundsList;
     public AudioSource backgroundMusic;
-    public Button settingsCloseButton;
-    public Scrollbar musicSetting;
-    public Scrollbar soundEffectsSetting;
 
     // Variables
     private List<AudioSource> sounds = new List<AudioSource>();
     private List<float> soundsDefaultVolume = new List<float>();
     private float backgroundMusicDefaultVolume;
     private bool oneTime = true;
-    public static float musicValue = 1;
-    public static float soundEffectsValue = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        settingsCloseButton.onClick.AddListener(delegate { setSettings(); });
-
         if (oneTime)
         {
             foreach (Transform audio in soundsList)
@@ -37,19 +29,7 @@ public class SettingsManager : MonoBehaviour
 
             oneTime = false;
         }
-    }
 
-    // Sets the current settings values
-    public void setSettings()
-    {
-        musicValue = musicSetting.value;
-        soundEffectsValue = soundEffectsSetting.value;
-        setMainSceneAudio();
-    }
-
-    // Changes the main scene's audio volume based on the settings
-    public void setMainSceneAudio()
-    {
         for (int i = 0; i < sounds.Count; i++)
         {
             sounds[i].volume = soundsDefaultVolume[i] * SettingsManager.soundEffectsValue;
