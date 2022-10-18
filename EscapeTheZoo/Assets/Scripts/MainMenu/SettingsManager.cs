667544a1f483ee4e6214822f83be7263e26a5218
cns3382 <cns3382@autuni.ac.nz>
2022-10-19 10:17:39 +1300

    Finished volume settings and most of credits
    Also added mute and unmute buttons, and the credits button image.

--- EscapeTheZoo/Assets/Scripts/MainMenu/SettingsManager.cs
+++ EscapeTheZoo/Assets/Scripts/MainMenu/SettingsManager.cs
@@ -8,53 +8,101 @@ public class SettingsManager : MonoBehaviour
     // Objects
     public Transform soundsList;
     public AudioSource backgroundMusic;
-    public Button settingsCloseButton;
     public Scrollbar musicSetting;
     public Scrollbar soundEffectsSetting;
+    public Button musicMuteButton;
+    public Button soundEffectsMuteButton;
+    public RectTransform emptyMusicContent;
+    public RectTransform emptySoundEffectsContent;
 
     // Variables
     private List<AudioSource> sounds = new List<AudioSource>();
-    private List<float> soundsDefaultVolume = new List<float>();
-    private float backgroundMusicDefaultVolume;
-    private bool oneTime = true;
+    private static List<float> soundsDefaultVolume = new List<float>();
+    private static float backgroundMusicDefaultVolume;
+    private static bool oneTime = true;
     public static float musicValue = 1;
     public static float soundEffectsValue = 1;
+    private static bool musicMute = false;
+    private static bool soundEffectsMute = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        settingsCloseButton.onClick.AddListener(delegate { setSettings(); });
+        // Listeners
+        musicMuteButton.onClick.AddListener(delegate { muteMusic(); });
+        soundEffectsMuteButton.onClick.AddListener(delegate { muteSoundEffects(); });
+        musicSetting.onValueChanged.AddListener(delegate { changeMusicVolume(); });
+        soundEffectsSetting.onValueChanged.AddListener(delegate { changeSoundEffectsVolume(); });
 
-        if (oneTime)
+        // Sets the initial mute sprites
+        musicMuteButton.GetComponent<Image>().sprite = Resources.Load<Sprite>("Art/MainMenu/" + (musicMute ? "Mute" : "Unmute"));
+        soundEffectsMuteButton.GetComponent<Image>().sprite = Resources.Load<Sprite>("Art/MainMenu/" + (soundEffectsMute ? "Mute" : "Unmute"));
+
+        // Sets the scrollbar position
+        emptyMusicContent.localPosition = new Vector3(0, (emptyMusicContent.rect.height / 2) - (emptyMusicContent.rect.height * (musicValue < 0 ? musicValue * -1 : musicValue)), 0);
+        emptySoundEffectsContent.localPosition = new Vector3(0, (emptySoundEffectsContent.rect.height / 2) - (emptySoundEffectsContent.rect.height * (soundEffectsValue < 0 ? soundEffectsValue * -1 : soundEffectsValue)), 0);
+
+        // Sets a list of sounds
+        foreach (Transform audio in soundsList)
         {
-            foreach (Transform audio in soundsList)
+            sounds.Add(audio.GetComponent<AudioSource>());
+
+            if (oneTime) // Gets the default sound effect volume once
             {
-                sounds.Add(audio.GetComponent<AudioSource>());
                 soundsDefaultVolume.Add(audio.GetComponent<AudioSource>().volume);
             }
+        }
 
+        if (oneTime) // Gets the default music volume once
+        {
             backgroundMusicDefaultVolume = backgroundMusic.volume;
 
             oneTime = false;
         }
-    }
 
-    // Sets the current settings values
-    public void setSettings()
-    {
-        musicValue = musicSetting.value;
-        soundEffectsValue = soundEffectsSetting.value;
-        setMainSceneAudio();
-    }
-
-    // Changes the main scene's audio volume based on the settings
-    public void setMainSceneAudio()
-    {
+        // Sets the sound effects volume
         for (int i = 0; i < sounds.Count; i++)
         {
-            sounds[i].volume = soundsDefaultVolume[i] * SettingsManager.soundEffectsValue;
+            sounds[i].volume = soundsDefaultVolume[i] * (soundEffectsMute ? 0 : soundEffectsValue);
         }
 
-        backgroundMusic.volume = backgroundMusicDefaultVolume * SettingsManager.musicValue;
+        // Sets the music volume
+        backgroundMusic.volume = backgroundMusicDefaultVolume * (musicMute ? 0 : musicValue);
+    }
+
+    // Changes the mute status of the music settings
+    public void muteMusic()
+    {
+        musicMute = musicMute ? false : true;
+        musicMuteButton.GetComponent<Image>().sprite = Resources.Load<Sprite>("Art/MainMenu/" + (musicMute ? "Mute" : "Unmute"));
+        changeMusicVolume();
+    }
+
+    // Changes the mute status of the sound effects settings
+    public void muteSoundEffects()
+    {
+        soundEffectsMute = soundEffectsMute ? false : true;
+        soundEffectsMuteButton.GetComponent<Image>().sprite = Resources.Load<Sprite>("Art/MainMenu/" + (soundEffectsMute ? "Mute" : "Unmute"));
+        changeSoundEffectsVolume();
+    }
+
+    // Changes music volume when the scrollbar is changed
+    public void changeMusicVolume()
+    {
+        musicValue = musicMute ? musicSetting.value * -1 : musicSetting.value;
+        backgroundMusic.volume = backgroundMusicDefaultVolume * musicValue;
+    }
+
+    // Changes sound effects volume when the scrollbar is changed
+    public void changeSoundEffectsVolume()
+    {
+        soundEffectsValue = soundEffectsMute ? soundEffectsSetting.value * -1 : soundEffectsSetting.value;
+
+        for (int i = 0; i < sounds.Count; i++)
+        {
+            sounds[i].volume = soundsDefaultVolume[i] * soundEffectsValue;
+        }
+
+        backgroundMusic.volume = backgroundMusicDefaultVolume * musicValue;
     }
 }
--- EscapeTheZoo/Assets/Scripts/SoundManager.cs
+++ EscapeTheZoo/Assets/Scripts/SoundManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour
 {
@@ -10,31 +11,50 @@ public class SoundManager : MonoBehaviour
 
     // Variables
     private List<AudioSource> sounds = new List<AudioSource>();
-    private List<float> soundsDefaultVolume = new List<float>();
-    private float backgroundMusicDefaultVolume;
-    private bool oneTime = true;
+    private static List<float> soundsDefaultVolume = new List<float>();
+    private static float backgroundMusicDefaultVolume;
+    private static List<string> sceneList = new List<string>();
+    private static List<int> soundsListPosition = new List<int>();
+    private static int audioCount = 0;
 
-    // Start is called before the first frame update
-    void Start()
+    // Awake is called before the first frame update, and before start
+    void Awake()
     {
-        if (oneTime)
+        bool notLoadedSceneBefore = !(sceneList.IndexOf(SceneManager.GetSceneAt(SceneManager.sceneCount - 1).name) >= 0);
+
+        // Sets the current sounds list position
+        if (notLoadedSceneBefore)
         {
-            foreach (Transform audio in soundsList)
-            {
-                sounds.Add(audio.GetComponent<AudioSource>());
-                soundsDefaultVolume.Add(audio.GetComponent<AudioSource>().volume);
-            }
+            soundsListPosition.Add(audioCount);
 
-            backgroundMusicDefaultVolume = backgroundMusic.volume;
-
-            oneTime = false;
         }
 
+        // Sets the list of sounds
+        foreach (Transform audio in soundsList)
+        {
+            sounds.Add(audio.GetComponent<AudioSource>());
+
+            if (notLoadedSceneBefore)
+            {
+                soundsDefaultVolume.Add(audio.GetComponent<AudioSource>().volume);
+                audioCount++;
+            }
+        }
+
+        // Sets the sounds list position, background music default volume, and adds the current scene to the scene list
+        if (notLoadedSceneBefore)
+        {
+            backgroundMusicDefaultVolume = backgroundMusic.volume;
+            sceneList.Add(SceneManager.GetSceneAt(SceneManager.sceneCount - 1).name);
+        }
+
+        // Sets the sound effects volume based on the sound effects setting
         for (int i = 0; i < sounds.Count; i++)
         {
-            sounds[i].volume = soundsDefaultVolume[i] * SettingsManager.soundEffectsValue;
+            sounds[i].volume = soundsDefaultVolume[i + soundsListPosition[sceneList.IndexOf(SceneManager.GetSceneAt(SceneManager.sceneCount - 1).name)]] * SettingsManager.soundEffectsValue;
         }
 
+        // Sets the musics volume based on the music setting
         backgroundMusic.volume = backgroundMusicDefaultVolume * SettingsManager.musicValue;
     }
 }
