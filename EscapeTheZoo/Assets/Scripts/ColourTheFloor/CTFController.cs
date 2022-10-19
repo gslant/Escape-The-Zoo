76864fb80fc2d3882ea552db7cf85f2175a2cbf8
gslant <77141272+gslant@users.noreply.github.com>
2022-10-19 18:52:17 +1300

    Timer added

--- EscapeTheZoo/Assets/Scripts/ColourTheFloor/CTFController.cs
+++ EscapeTheZoo/Assets/Scripts/ColourTheFloor/CTFController.cs
@@ -15,18 +15,26 @@ public class CTFController : MonoBehaviour
 
     public GameObject grid;
     public GameObject squarePrefab;
+    private List<GameObject> squareList;
 
     public KeyCode testKey;
     float powerUpStartTime;
+
+    public TextMeshProUGUI timerText;
+    private float currentTime;
+    private float startingTime = 60f;
     // Start is called before the first frame update
     void Start()
     {
+
+        currentTime = startingTime;
+        timerText.SetText(startingTime.ToString());
         GameOverCanvas.SetActive(false);
         for (float j = -8.5f; j < 9f; j += 0.5f)
         {
-            for (float i = -4.5f; i <= 4.5f; i += 0.5f)
+            for (float i = -4.5f; i <= 3.5f; i += 0.5f)
             {
-                Instantiate(squarePrefab, new Vector3(j, i, 0), Quaternion.identity);
+                squareList.Add(Instantiate(squarePrefab, new Vector3(j, i, 0), Quaternion.identity));
             }
         }
     }
@@ -34,6 +42,7 @@ public class CTFController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        currentTime -= 1f * Time.deltaTime;
+        timerText.SetText(((int) currentTime).ToString());
     }
 }
