40603c703fa583bfeccf059fb8aab405040a63a4
dtm3080 <dtm3080@autuni.ac.nz>
2022-10-24 14:02:37 +1300

    declaring each powerup

---
+++ EscapeTheZoo/Assets/Scripts/Powerups/PowerupItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupItem : MonoBehaviour
+{
+    public string Title;
+    public string desc;
+    public Sprite sprite;
+}
